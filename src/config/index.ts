43c096d356c0c0525bf1a72ce2f922d508ecab85
Hamed Abdullah <hamedabdullahmachrur@gmail.com>
2026-05-22 15:24:31 +0600

    login user and jwt added

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ import { env } from 'process';
 dotenv.config({quiet: true})
 const config ={
     port: env.PORT,
-    db_url: env.DATABASE_URL
+    db_url: env.DATABASE_URL,
+    JWT_SECRET: env.JWT_SECRET
 }
 export default config
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,6 +4,8 @@ import { StatusCodes } from "http-status-codes";
 import type { SafeUser, SignupBody } from "../../types";
 import { queryOne } from "../../utils/dbQuery";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import config from "../../config";
 
 
 export const signup = async (req: Request, res: Response) => {
@@ -34,11 +36,47 @@ export const signup = async (req: Request, res: Response) => {
       INSERT INTO users (name, email, password, role)
       VALUES ($1, $2, $3, $4)
       RETURNING id, name, email, role, created_at, updated_at
-    `,[name, email, hashPassword, role])
+    `, [name, email, hashPassword, role])
   sendSuccess(res, newUser, "User created successfully", StatusCodes.CREATED)
 }
 
-const login = (req: Request, res: Response) => {
+const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    sendError(res, "Email and password are required.", StatusCodes.BAD_REQUEST)
+  }
+
+  const user = await queryOne<SafeUser & { password: string }>(
+    `SELECT * FROM users WHERE email=$1`,
+    [email]
+  )
+
+  if (!user) {
+    sendError(res, "Invalid email or password.", StatusCodes.UNAUTHORIZED)
+  }
+
+  const isMatch = await bcrypt.compare(password, user?.password as string)
+
+  if (!isMatch) {
+    sendError(res, "Invalid password.", StatusCodes.UNAUTHORIZED)
+  }
+
+  const userInfo = {
+    id: user?.id, 
+    name: user?.name, 
+    email: user?.email,
+    role: user?.role ,
+    created_at:user?.created_at,
+    updated_at:user?.updated_at
+  }
+
+  const token = jwt.sign(
+    userInfo,
+    config.JWT_SECRET as string,
+    { expiresIn: '7d' }
+  );
+
+  sendSuccess(res, { token, user :userInfo }, 'Login successful');
 
 }
 export const authController = {
