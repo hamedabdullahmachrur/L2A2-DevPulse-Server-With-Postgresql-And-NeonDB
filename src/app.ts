15059efb66a5e1297928326decebe8259277d1f4
Hamed Abdullah <hamedabdullahmachrur@gmail.com>
2026-05-23 22:08:28 +0600

    add cors to app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { type Application } from "express"
 import { authRouter } from "./modules/auth/auth.route";
 import { issuesRoute } from "./modules/issues/issues.route";
 import { errorHandler } from "./middlewere/errorHandler";
+import cors from 'cors'
 
 const app: Application = express()
 
@@ -9,6 +10,7 @@ app.get('/', (req, res) =>{
     res.send("This is DevPulse server")
 })
 app.use(express.json())
+app.use(cors());
 
 // Routes
 app.use('/api/auth', authRouter);
